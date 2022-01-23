8445c287063a29175c0517d93c51addd894f4e8d
Mohamad Hamade <mohamadhamade30@gmail.com>
2022-01-23 20:32:36 +0200

    cleaned code

--- main.js
+++ main.js
@@ -1,42 +1,9 @@
-//the key of the key
+//the key of the token in local storage
 //196b95d9-c2bb-4549-8ee0-128a264df39f
 
-//the value of the token
-//8e102823-bb14-451a-bdd6-65d7d7a8919b
-
-const queryString = window.location.href;
-
 function saveToken() {
-    console.log(queryString);
-    var index;
-    for (let i = 0; i < queryString.length; i++) {
-        console.log(queryString[i]);
-        if (queryString[i] == "=") {
-            index = i;
-            console.log(index)
-            break;
-        }
-    }
-    var token = queryString.substring(index + 1);
-    console.log(token)
-    // I can only set 1 token with key employee, if I set another one
-    //with same key it will be overriden.
+    let params = new URLSearchParams(location.search);
+    var token = params.get('id')
     localStorage.setItem('196b95d9-c2bb-4549-8ee0-128a264df39f', token)
 }
-
-
-function updateUI() {
-    var values = [], keys = Object.keys(localStorage), i = keys.length;
-    while (i--) { values.push(localStorage.getItem(keys[i])); }
-    document.getElementById('ls-currently').textContent = values;
-}
-
 saveToken();
-
-//local storage is supported by major browsers but we can still check
-
-// if (typeof (Storage) !== "undefined") {
-
-// } else {
-
-// }
